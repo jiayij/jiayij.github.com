cac063a89b63ba9ef24d3e7740821e3b0eb5a5d5
Jiayi Jiang <bjyjiang@ucdavis.edu>
2021-03-10 09:29:30 -0800

    update

--- des157/final/usertest/script.js
+++ des157/final/usertest/script.js
@@ -2,6 +2,6 @@
     'use strict';
     console.log("reading js");
 
-    alert("Welcome! Please test the following:\n1. Hover over the images on the home page before clicking on them\n2. Click on the images inside the pages.\n3. Test the navigation links inside the pages.");
+    alert("Welcome! Please test the following:\n1. Click on the color block \n2. Click on the circle \n3. Test the animation for the photograph.");
 
 })();
